304abb573db4f606d539f604209a87792a1876e1
fjcastanedap96 <fjcastanedap@gmail.com>
2025-03-12 16:30:04 -0500

    pruebas de funcion

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo () {
+    nombreAmigo = document.getElementById("amigo").value ;
+    if (nombreAmigo == "") {
+        alert("No has digitado un Nombre!");
+        exit();
+    }
+    alert(nombreAmigo)
+}
+
+
+
+function sortearAmigo () {
+    document.getElementById
+}
