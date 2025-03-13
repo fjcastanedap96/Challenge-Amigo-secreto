5f5aebc43259d53f3c3fa7b7677aa4e4dd6c2185
fjcastanedap96 <fjcastanedap@gmail.com>
2025-03-13 16:02:12 -0500

    Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li>

--- app.js
+++ app.js
@@ -4,21 +4,44 @@ let amigos = []
 // Funcion para agregar amigos
 function agregarAmigo () {
     // Capturar el valor del campo de entreda
-    nombreAmigo = document.getElementById("amigo").value ;
+    let nombreAmigo = document.getElementById("amigo").value ;
     
     // validar la entrada
     if (nombreAmigo == "") {
         alert("Por favor, inserte un nombre.");
-        exit();
+        return 0;
     }
 
     // Actualizar el array de amigos
     amigos.push(nombreAmigo);
-    alert("Se almaceno con exito.");
+    //alert("Se almaceno con exito.");
 
     // limpiar el campo de entreda
     document.getElementById("amigo").value = '';
 
     // log
     console.log(amigos)
+
+    agregarAmigosLista();
+
+}
+
+function agregarAmigosLista () {
+    let codigo = "";
+
+    // Obtener el elemento de la lista
+    let lista = document.getElementById("listaAmigos");
+    console.log(lista)
+
+    // Limpiar la lista existente
+    lista.innerHTML = "";
+
+    // Iterar sobre el arreglo
+    for (i = 0 ; i < amigos.length; i++) {
+        codigo = codigo + "<li>" + amigos[i] + "<li/>";
+    }
+    // Agregar elementos a la lista
+    lista.innerHTML = codigo;
+    return 0;
+    
 }
